334f0bc26b6e384179c2d219b488c9ba21a96613
saoloh <solahyasser@gmail.com>
2025-04-20 12:49:09 +0200

    final solution

---
+++ section11/script.js
@@ -0,0 +1,13 @@
+// let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+// let index = 0;
+// let counter = 0;
+// while(counter < friends.length){
+//     if(friends[counter][index] !== 'A' && typeof(friends[counter]) !== typeof(counter)){
+//         console.log(`"${counter} => ${friends[counter]}"`)
+//     }
+
+//     counter++;
+// }
+// // Output
+// "1 => Sayed"
+// "2 => Mahmoud"
