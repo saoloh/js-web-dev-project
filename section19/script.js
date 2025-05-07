b5e335278c5323d049317b912166407a1c80020d
saoloh <solahyasser@gmail.com>
2025-05-07 13:48:22 +0200

    solution

---
+++ section19/script.js
@@ -0,0 +1,58 @@
+
+/* Bom challenge*/
+
+// tasksDiv = document.querySelector(".container .task");
+// taskName = document.querySelector(".input");
+// let tasks = [];
+// let i =0 ;
+// let ele = document.createElement("div");
+// let elep = document.createElement("p");
+// let eleButton = document.createElement("button");
+
+// eleButton.textContent = "remove";
+// ele.appendChild(elep);
+// ele.appendChild(eleButton);
+// submit = document.querySelector(".container .form .add").addEventListener("click",function(){
+//     i++;
+//     tasks.push({title:taskName.value});
+//     window.localStorage.setItem("tasks",tasks);
+//     elep.textContent =`${taskName.value}`;
+//     eleButton.setAttribute("class",`remove-button${i}`);
+//     tasksDiv.appendChild(ele.cloneNode(true));
+
+
+//     //turn the delet button
+//     let removebtn = document.querySelectorAll("button");
+//     console.log(removebtn);
+//     let btnArr = Array.from(removebtn);
+//     btnArr.forEach(element => {
+//         element.addEventListener("click",function(){
+//         this.parentElement.remove();
+//     });
+//     });
+  
+// })
+
+
+//=======================================================
+// let fontOption = document.querySelector("#font").onclick = function (){
+//     window.localStorage.setItem("font",this.value);
+// }
+// let colorOption = document.querySelector("#color").onclick = function (){
+//     window.localStorage.setItem("color",this.value);
+// }
+// let valueOption = document.querySelector("#value").onclick = function (){
+//     window.localStorage.setItem("value",this.value);
+// }
+//=======================================================
+// let textInput = document.querySelector("#textbox").onblur = function (){
+//     window.sessionStorage.setItem("text",this.value);}
+
+// let passwordinput = document.querySelector("#passwordbox").onblur = function (){
+//     window.sessionStorage.setItem("password",this.value);}
+
+// let searchinput = document.querySelector("#searchbox").onblur = function (){
+//     window.sessionStorage.setItem("search",this.value);}
+
+// let colorinput = document.querySelector("#coloroption").onclick = function (){
+//     window.sessionStorage.setItem("color",this.value);}
