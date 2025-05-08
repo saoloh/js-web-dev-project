84e97cdac9d2c81b585c727c369c5613ec087849
saoloh <solahyasser@gmail.com>
2025-05-08 16:45:01 +0200

    higer order function

--- section10/script.js
+++ section10/script.js
@@ -1,126 +1,54 @@
-// let start = 10;
-// let end = 100;
-// let exclude = 40;
-
-// for(let i = start; i <= end; i+=start){
-//     if(i !== 40){
-//         console.log(i);
-//     } else{
-//         continue;
-//     }
-// }
-// // Output
-// 10
-// 20
-// 30
-// 50
-// 60
-// 70
-// 80
-// 90
-// 100
-//============================================
-// let start = 10;
-// let end = 0;
-// let stop = 3;
-// mainLoop:for(let i = start; i >= stop;i--){
-//     if(i < 10){
-//         console.log(end + i);
-//     } else{
-//         console.log(i);
-//     }
+// let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
+// let newArr = mix.map((ele)=>{
+//     if(typeof(ele)==="string"){
+//         return ele;
+//     }else{
+//         return "";
+//     } 
+// }).reduce((acc,cur)=>{
+//     if(typeof(cur)==="string"){
+//         return acc + cur;
+//     } 
+// }).trim();
+// console.log(newArr);
+// Elzero
+//===================================
+// let myString = "EElllzzzzzzzeroo";
+// let newArr = myString.split("").filter((ele,index)=>{
     
-// }
-// Output
-// 10
-// 09
-// 08
-// 07
-// 06
-// 05
-// 04
-// 03
-//======================================
-// let start = 1;
-// let end = 6;
-// let breaker = 2;
-// for(let i = start ; i <= end ; i++){
-//     console.log(i);
-//     console.log("--",breaker);
-//     console.log("--", end - breaker );
-// }
-// Output
-// 1
-// -- 2
-// -- 4
-// 2
-// -- 2
-// -- 4
-// 3
-// -- 2
-// -- 4
-// 4
-// -- 2
-// -- 4
-// 5
-// -- 2
-// -- 4
-// 6
-// -- 2
-// -- 4
-//==========================
-// let index = 10;
-// let jump = 2;
-
-// for (;;) {
-//     console.log(index)
-//     index-= jump;
-//   if(index ===(jump) )break;
-// }
-
-// ==================================
-// let friends = ["Ahmed", "Sayed", "Eman", "Mahmoud", "Ameer", "Osama", "Sameh"];
-// let letter = "a";
-// for(let i = letter.length; i < friends.length;i++){
-//     if(friends[i][letter.length - letter.length ] === letter.toUpperCase()){
-//         continue;
+//     if(index == 0){
+//         return ele;
+//     } else if(ele !== myString[index-1]){
+//         return ele;
+//     } 
+// }).join("");
+// console.log(newArr);
+// // Elzero
+//==================================
+// let myArray = ["E", "l", "z", ["e", "r"], "o"];
+// let newArr = myArray.reduce((acc,cur)=>{
+//     if(typeof(cur) !== "string"){
+//     cur = cur.join("");
+//     }
+//     return acc + cur;
+// });
+// console.log(newArr);
+// Elzero
+//========================================
+// let numsAndStrings = [1, 10, -10, -20, 5, "A", 3, "B", "C"];
+// let newArr = numsAndStrings.filter(ele=>{
+//     return !isNaN(ele);
+// }).map(ele=>ele*-1);
+// console.log(newArr);
+// // [-1, -10, 10, 20, -5, -3]
+//========================================
+// let nums = [2, 12, 11, 5, 10, 1, 99];
+// let newArr = nums.reduce((acc,cur)=>{
+//     if(cur%2 === 0){
+//         return acc *cur 
 //     } else{
-//         console.log(`"${i} => ${friends[i]}"`);
+//         return acc + cur
 //     }
-// }
-// Output
-// "1 => Sayed"
-// "2 => Eman"
-// "3 => Mahmoud"
-// "4 => Osama"
-// "5 => Sameh"
-// ==================================
-// let start = 0;
-// let swappedName = "elZerO";
-// for(let i = start; i < swappedName.length; i++){
-//     if(swappedName.charAt(i)  ){//ASCII CODE
-//         console.log(swappedName.charAt(i).toUpperCase)
-//     } else{
-//         console.log(swappedName.charAt(i).toLowerCase)
-//     }
-// }
-// // Output
-// "ELzERo"
-//===============
-// let start = 0;
-
-// let mix = [1, 2, 3, "A", "B", "C", 4];
-// for(let i = start; i < mix.length; i++){
-//     if(i === start){
-//         continue;
-//     }
-//     if(typeof(mix[i]) === typeof(mix[start])){
-//         console.log(mix[i]);
-//     }
-
-
-// }
-// Output
-// 2
-// 3
-// 4
+// },1)
+// console.log(newArr);
+// 500
