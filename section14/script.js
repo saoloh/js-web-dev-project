97d28d72822f341dc2b60239f396f5a9174688fc
saoloh <solahyasser@gmail.com>
2025-05-08 16:48:55 +0200

    Bom

--- section14/script.js
+++ section14/script.js
@@ -1,54 +1,64 @@
-// let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
-// let newArr = mix.map((ele)=>{
-//     if(typeof(ele)==="string"){
-//         return ele;
-//     }else{
-//         return "";
-//     } 
-// }).reduce((acc,cur)=>{
-//     if(typeof(cur)==="string"){
-//         return acc + cur;
-//     } 
-// }).trim();
-// console.log(newArr);
-// Elzero
-//===================================
-// let myString = "EElllzzzzzzzeroo";
-// let newArr = myString.split("").filter((ele,index)=>{
-    
-//     if(index == 0){
-//         return ele;
-//     } else if(ele !== myString[index-1]){
-//         return ele;
-//     } 
-// }).join("");
-// console.log(newArr);
-// // Elzero
-//==================================
-// let myArray = ["E", "l", "z", ["e", "r"], "o"];
-// let newArr = myArray.reduce((acc,cur)=>{
-//     if(typeof(cur) !== "string"){
-//     cur = cur.join("");
+// let promptmsg = prompt("print numbers from-to","Example 5-20");
+
+// console.log(promptmsg);
+// let hyphenIndex =  promptmsg.indexOf("-");
+// let firstNumber = promptmsg.slice(0,hyphenIndex);
+// let lastNumber = promptmsg.slice(hyphenIndex+1);
+
+// if(firstNumber < lastNumber){
+//     for(let i =firstNumber;i <= lastNumber;i++){
+//         console.log(i);
 //     }
-//     return acc + cur;
-// });
-// console.log(newArr);
-// Elzero
-//========================================
-// let numsAndStrings = [1, 10, -10, -20, 5, "A", 3, "B", "C"];
-// let newArr = numsAndStrings.filter(ele=>{
-//     return !isNaN(ele);
-// }).map(ele=>ele*-1);
-// console.log(newArr);
-// // [-1, -10, 10, 20, -5, -3]
-//========================================
-// let nums = [2, 12, 11, 5, 10, 1, 99];
-// let newArr = nums.reduce((acc,cur)=>{
-//     if(cur%2 === 0){
-//         return acc *cur 
-//     } else{
-//         return acc + cur
+// } else {
+//     for(let i =lastNumber;i <= firstNumber;i++){
+//         console.log(i);
 //     }
-// },1)
-// console.log(newArr);
-// 500
+// }
+//=====================================================
+
+// let counter = setTimeout(function (){
+// let popelement = document.createElement("div");
+// console.log(popelement);
+// popelement.innerHTML="<h3>welcome</h3><p>welcome to blah blah</p><div>x</div>";
+// console.log(popelement);
+// popelement.style.cssText = "width: 300px;background-color: black;color: white;transform: translateY(50%);margin: auto;display: flex;justify-content: center;align-items: center;flex-direction: column;"
+// popelement.children[2].style.cssText = "width:50px;height:50px;border-raduis:50px;background-color:blue;"
+// document.body.appendChild(popelement);
+// console.log(popelement.children[2]);
+// popelement.children[2].onClick = function(){
+//     popelement.children[2].parentElement.remove();
+// }
+// },5000);
+//=================================================
+// let ele = document.querySelector("div");
+// console.log(ele);
+// function a (){
+//     ele.innerHTML -= 1;
+//     if(ele.innerHTML === "0"){
+//         clearInterval(counter);
+//     }
+// }
+// let counter = setInterval(a,1000);
+//==============================================
+// let ele = document.querySelector("div");
+// console.log(ele);
+// function a (){
+//     ele.innerHTML -= 1;
+//     if(ele.innerHTML === "0"){
+//         location.href = "Elzero.org";
+//     }
+// }
+// let counter = setInterval(a,1000);
+//==============================================
+// let ele = document.querySelector("div");
+// console.log(ele);
+// function a (){
+//     ele.innerHTML -= 1;
+//     if(ele.innerHTML === "5"){
+//         window.open("Elzero.org","_blank","width:300,height:300") ;
+//     }
+//     if(ele.innerHTML === "0"){
+//         clearInterval(counter);
+//     }
+// }
+// let counter = setInterval(a,1000);
