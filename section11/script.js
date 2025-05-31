63c185ddc866c28d1d0165faa335a166f81e5c52
mostafa mohamed abobakr <mo18afa8moh2004@gmail.com>
2025-05-31 22:03:30 +0300

    solution

---
+++ section11/script.js
@@ -0,0 +1,28 @@
+let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+let index = 0;
+let counter = 0;
+
+while (index < friends.length)
+{
+    if (
+            typeof(friends[index]) !== "number" &&
+            friends[index][+false] !== friends[+false][+false]
+    )
+    {
+        console.log(`"${counter+true} => ${friends[index]}"`);
+        counter++;
+    }
+        index++;
+
+}
+
+// while (index < friends.length) {
+//     if (typeof friends[index] !== "number" && !friends[index].startsWith("A")) {
+//         console.log(`${counter + true} => ${friends[index]}`);
+//         counter++;
+//     }
+//     index++;
+// }
+// Output
+("1 => Sayed");
+("2 => Mahmoud");
